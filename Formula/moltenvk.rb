7aa4833c325de18457b4f4946be7f6014e3f4da1
Rafael Amador Galvan <rafael.amador@gmail.com>
2019-02-05 11:23:59 -0600

    Adjusted MoltenVK dependencies and added spirv-cross Formula

--- Formula/moltenvk.rb
+++ Formula/moltenvk.rb
@@ -4,25 +4,18 @@ class Moltenvk < Formula
   url "https://github.com/KhronosGroup/MoltenVK/archive/v1.0.32.tar.gz"
   sha256 "43538642e604976883e86462a52f571e6aa7576fe5f4f0d3bee38227bca8c086"
   head "https://github.com/KhronosGroup/MoltenVK.git", :tag => "v1.0.32"
+  depends_on "cereal" => :build
   depends_on "cmake" => :build
   depends_on "ninja" => :build
   depends_on "python" => :build
   depends_on "vulkan-headers" => :build
   depends_on "vulkan-portability" => :build
-  depends_on "cereal"
+  depends_on "spirv-cross"
   depends_on "spirv-tools"
 
-  # resource "Vulkan-Headers" do
-  #   url "https://github.com/KhronosGroup/Vulkan-Headers.git", :commit => "c200cb25db0f47364d3318d92c1d8e9dfff2fef1"
+  # resource "SPIRV-Cross" do
+  #   url "https://github.com/KhronosGroup/SPIRV-Cross.git", :commit => "a029d3faa12082bb4fac78351701d832716759df"
   # end
-
-  # resource "Vulkan-Portability" do
-  #   url "https://github.com/KhronosGroup/Vulkan-Portability.git", :commit => "53be040f04ce55463d0e5b25fd132f45f003e903"
-  # end
-
-  resource "SPIRV-Cross" do
-    url "https://github.com/KhronosGroup/SPIRV-Cross.git", :commit => "a029d3faa12082bb4fac78351701d832716759df"
-  end
 
   resource "glslang" do
     url "https://github.com/KhronosGroup/glslang.git", :commit => "2898223375d57fb3974f24e1e944bb624f67cb73"
