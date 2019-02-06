abef033ee0109beff225c077f223350c6811acb2
Rafael Amador Galvan <rafael.amador@gmail.com>
2019-02-06 09:34:07 -0600

    adjustments to inreplace

--- Formula/moltenvk.rb
+++ Formula/moltenvk.rb
@@ -41,8 +41,8 @@ class Moltenvk < Formula
       s.gsub! '"\"$(SRCROOT)/glslang/External/spirv-tools/include\"",', '"\"' + "#{HOMEBREW_PREFIX}/include/" +'\"",'
       s.gsub! '"\"$(SRCROOT)/glslang/External/spirv-tools/external/spirv-headers/include\"",', '"\"' + "#{HOMEBREW_PREFIX}/include/spirv/" +'\"",'
       s.gsub! '"\"$(SRCROOT)/glslang/build/External/spirv-tools\"",', '"\"' + "#{HOMEBREW_PREFIX}/include/glslang/" +'\"",'
-      s.gsub! "path = ../External/build/macOS/libSPIRVTools.a;", "path = #{HOMEBREW_PREFIX}/lib/libSPIRV-Tools.a;"
-      s.gsub! "path = ../External/build/macOS/libSPIRVCross.a;", "path = #{HOMEBREW_PREFIX}/lib/libspirv-cross-core.a;"
+      s.gsub! "name = libSPIRVTools.a; path = ../External/build/macOS/libSPIRVTools.a;", "name = libSPIRV-Tools.a; path = #{HOMEBREW_PREFIX}/lib/libSPIRV-Tools.a;"
+      s.gsub! "name = libSPIRVCross.a; path = ../External/build/macOS/libSPIRVCross.a;", "name = libspirv-cross-core.a; path = #{HOMEBREW_PREFIX}/lib/libspirv-cross-core.a;"
       s.gsub! "path = ../External/build/macOS/libglslang.a;", "path = #{HOMEBREW_PREFIX}/lib/libglslang.a;"
     end
 
