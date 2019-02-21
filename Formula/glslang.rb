d07e4214a1684b8ca90f1292a40245bad87af042
Rafael Amador Galvan <rafael.amador@gmail.com>
2019-02-21 16:02:48 -0600

    Adding parameter to link SPIRV-Tools dynamic library from cmake in glslang formulae

--- Formula/glslang.rb
+++ Formula/glslang.rb
@@ -13,12 +13,12 @@ class Glslang < Formula
   def install
     # Disabling Tests for now
     args = std_cmake_args + [
-      "-DBUILD_SHARED_LIBS=ON",
+      "-DBUILD_SHARED_LIBS=OFF",
       "-DCMAKE_CXX_FLAGS=-I#{HOMEBREW_PREFIX}/include",
       "-DCMAKE_STATIC_LINKER_FLAGS=#{Formula["spirv-tools"].opt_lib}/libSPIRV-Tools.a",
+      "-DCMAKE_SHARED_LINKER_FLAGS=#{Formula["spirv-tools"].opt_lib}/libSPIRV-Tools-Shared.dylib",
       "-DCMAKE_EXE_LINKER_FLAGS=-L#{HOMEBREW_PREFIX}/lib",
       "-DENABLE_GLSLANG_BINARIES=OFF",
-      "-DBUILD_SHARED_LIBS=OFF",
     ]
 
     inreplace Dir["#{buildpath}/CMakeLists.txt"].each do |s|
