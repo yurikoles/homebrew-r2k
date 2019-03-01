faf837648f9f8beccb5fa26e031fe69955378f00
Rafael Amador Galvan <rafael.amador@gmail.com>
2019-02-28 19:01:10 -0600

    Adding spirv-cross.pc file to spirv-cross Formula

--- Formula/spirv-cross.rb
+++ Formula/spirv-cross.rb
@@ -9,25 +9,44 @@ class SpirvCross < Formula
   depends_on "rafaga/r2k/spirv-headers" => :build
   conflicts_with "homebrew/core/spirv-cross"
 
-  # resource "SPIRV-Headers" do
-  #   url "https://github.com/KhronosGroup/SPIRV-Headers.git", :using => :git, :commit => "8bea0a266ac9b718aa0818d9e3a47c0b77c2cb23"
-  # end
-
   def install
-    # Disabling Tests for now
-    # args = std_cmake_args + %w[
-    #   -DSPIRV_BUILD_COMPRESSION=ON
-    #   -DSPIRV-Headers_SOURCE_DIR="#{HOMEBREW_PREFIX}"
-    #   -DSPIRV_SKIP_TESTS=ON
-    # ]
-    # resources.each do |resource|
-    #   resource.stage buildpath/"external"/resource.name
-    # end
+    pc_file_name = "#{buildpath}/spirv-cross.pc.cmake.in"
+
+    inreplace Dir["#{buildpath}/CMakeLists.txt"].each do |s|
+      s.gsub! "enable_testing()", \
+              "enable_testing()\n" \
+              "include(GNUInstallDirs)"
+      s.gsub! "target_link_libraries(spirv-cross-cpp spirv-cross-glsl)", \
+              "target_link_libraries(spirv-cross-cpp spirv-cross-glsl)\n\n" \
+              "configure_file(" \
+              "${CMAKE_CURRENT_SOURCE_DIR}/spirv-cross.pc.cmake.in " \
+              "${CMAKE_CURRENT_BINARY_DIR}/spirv-cross.pc " \
+              "@ONLY)\n" \
+              "install(" \
+              "FILES ${CMAKE_CURRENT_BINARY_DIR}/spirv-cross.pc " \
+              "DESTINATION ${CMAKE_INSTALL_LIBDIR}/pkgconfig" \
+              ")"
+    end
 
     mkdir "build" do
+      File.write(pc_file_name, pc_file)
       system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
+  end
+
+  def pc_file; <<~EOS
+    prefix=@CMAKE_INSTALL_PREFIX@
+    exec_prefix=@CMAKE_INSTALL_PREFIX@
+    libdir=${exec_prefix}/@CMAKE_INSTALL_LIBDIR@
+    includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@
+    \nName: @PROJECT_NAME@
+    Description: Tool for parsing and converting SPIR-V to other shader language
+    Requires:
+    Version: 2019-02-20
+    Libs: -L${libdir} -lspirv-cross-core -lspirv-cross-cpp -lspirv-cross-glsl -lspirv-cross-hlsl -lspirv-cross-msl -lspirv-cross-reflect -lspirv-cross-util
+    Cflags: -I${includedir}
+  EOS
   end
 
   test do
